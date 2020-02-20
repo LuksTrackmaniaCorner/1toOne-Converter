c1757b0f3a04a7ad7b4e3a4140167e029d5ae3cd
LuksTrackmaniaCorner <throwawayaccount1@web.de>
2020-02-20 17:35:01 +0100

    updated propositions

--- 1toOne Converter/src/Propositions.cs
+++ 1toOne Converter/src/Propositions.cs
@@ -1,4 +1,7 @@
-﻿//TODO Carry over mediatracker
+﻿//TODO Speed sign pillars
+//TODO Carry over mediatracker
 //TODO Support united envimixing
-//TODO Move thumbnail camera position
+//TODO Move thumbnail camera position (correctly)
 //TODO improve blockmixing with terrain items
+//TODO proper gui
+//TODO Investigate into maps with muliple starts
