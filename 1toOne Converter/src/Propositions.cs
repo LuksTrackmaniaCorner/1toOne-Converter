cca5349a37e14392274bfa8fa5073a6216251166
LuksTrackmaniaCorner <throwawayaccount1@web.de>
2020-03-28 21:24:26 +0100

    Removed output paths from the proposition menu.

--- 1toOne Converter/src/Propositions.cs
+++ 1toOne Converter/src/Propositions.cs
@@ -1,10 +1,8 @@
-﻿//TODO Speed sign pillars
-//TODO Carry over mediatracker
+﻿//TODO Carry over mediatracker
 //TODO Support united envimixing
 //TODO Move thumbnail camera position (correctly) - manual adjustment needed?
 //TODO Improve blockmixing with terrain items
 //TODO Proper gui
 //     - Make it possible to more maps after the converter has finished
-//     - Special output paths depending on the environment
 //     - Settings menu
 //TODO Investigate into maps with muliple starts
